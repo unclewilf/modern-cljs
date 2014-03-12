850622f50b88676659368517bb9e5bc8d57d9821
funnellw <Will.Funnell@digitaslbi.com>
2014-03-12 14:02:41 +0000

    Initial setup and sbuild file

---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  ;; CLJ AND CLJS source code path
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  ;; lein-cljsbuild plugin to build a CLJS project
+  :plugins [[lein-cljsbuild "1.0.0"]]
+
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
